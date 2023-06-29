fe4c409157d2c8048ff9b683697c54f3f1db47bd
Josue Morales <molinajosue92@hotmail.com>
2023-06-29 16:57:48 -0400

    add enum and print out headers

--- factory_api/src/api/routes/webhook.rs
+++ factory_api/src/api/routes/webhook.rs
@@ -1,10 +1,27 @@
-use actix_web::{post, web::Json, HttpResponse};
+use actix_web::{post, web::Json, HttpRequest, HttpResponse};
 use serde_json::Value;
 
 use crate::api::error::ApiError;
 
+pub enum GithubEvents {
+    /// This event occurs when there is activity relating to which repositories a GitHub App installation can access.
+    /// All GitHub Apps receive this event by default. You cannot manually subscribe to this event.
+    InstallationRepositories,
+    /// This event occurs when there is activity relating to a GitHub App installation.
+    /// All GitHub Apps receive this event by default. You cannot manually subscribe to this event
+    Installation,
+    /// This event occurs when a commit or tag is pushed.
+    Push,
+    /// This event occurs when there is activity on a pull request.
+    PullRequest,
+}
+
 #[post("/github/webhook")]
-pub async fn github_webhook(payload: Json<Value>) -> Result<HttpResponse, ApiError> {
+pub async fn github_webhook(
+    payload: Json<Value>,
+    req: HttpRequest,
+) -> Result<HttpResponse, ApiError> {
     println!("{}", serde_json::to_string_pretty(&payload).unwrap());
+    println!("{:?}", req.headers());
     Ok(HttpResponse::Ok().finish())
 }
